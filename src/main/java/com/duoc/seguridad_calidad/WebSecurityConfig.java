c5bf202b945d37714f2a76a13cbf0f8f735cf841
pedro falfan <pe.falfan@duocuc.cl>
2024-11-04 23:34:29 -0300

    resolved import error

--- src/main/java/com/duoc/seguridad_calidad/WebSecurityConfig.java
+++ src/main/java/com/duoc/seguridad_calidad/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.duoc.seguridad_calidad;
 
+import com.duoc.seguridad_calidad.security.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
@@ -8,7 +10,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
