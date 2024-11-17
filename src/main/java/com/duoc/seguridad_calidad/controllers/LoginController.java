df31f7aee9872b0fd8405d6813f7a8c780d2aff4
pedro falfan <pe.falfan@duocuc.cl>
2024-11-17 00:35:32 -0300

    base code for login

--- src/main/java/com/duoc/seguridad_calidad/controllers/LoginController.java
+++ src/main/java/com/duoc/seguridad_calidad/controllers/LoginController.java
@@ -1,19 +1,21 @@
 package com.duoc.seguridad_calidad.controllers;
 
+import com.duoc.seguridad_calidad.models.ResponseModel;
+import com.duoc.seguridad_calidad.models.UserModel;
 import com.duoc.seguridad_calidad.security.TokenStore;
+import com.google.gson.Gson;
+import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
 
 @Controller
 public class LoginController {
@@ -34,66 +36,39 @@ public class LoginController {
     }
 
     @PostMapping("/ingresar")
-    public String ingresar(@RequestParam(name = "email") String email, @RequestParam(name = "password") String password) {
-        System.out.println("EN INGRESAR" + email);
-        System.out.println("EN INGRESAR" + password);
+    public String ingresar(@RequestParam(name = "email") String email, @RequestParam(name = "password") String password, Model model) {
 
-        String url = "http://localhost:8081/login";
+        String url = "http://localhost:8081/api/login";
 
         final var restTemplate = new RestTemplate();
 
-        // Agregar parámetros a la URL
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
-                .queryParam("user", email)
-                .queryParam("encryptedPass", password);
+                .queryParam("correo", email)
+                .queryParam("password", password);
 
-//        String token = tokenStore.getToken();
-
-//        System.out.println("EN INGRESAR TOKEN" + token);
-
-        // Crear los encabezados y añadir el token
         HttpHeaders headers = new HttpHeaders();
-//        headers.set("Authorization", token);
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        HttpEntity<String> entity = new HttpEntity<>(
+                "parameters", headers
+        );
 
-        // Realizar la petición con el token en el encabezado y los parámetros en la URL
-        ResponseEntity response = restTemplate.exchange(builder.toUriString(), HttpMethod.POST, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.POST, entity, String.class);
 
-        System.out.println("Response codigo: " + response.getStatusCode());
-        System.out.println("Response body: " + response.getBody());
-//        System.out.println("Response: " + response);
+        ResponseModel responseModel = new Gson().fromJson(response.getBody(), ResponseModel.class);
+
+        JSONObject jsonObject = new JSONObject(responseModel);
+
+        UserModel user = new Gson().fromJson(String.valueOf(jsonObject.getJSONObject("data").getJSONObject("user")), UserModel.class);
+
+        System.out.println("usuario: " + user.getNombre());
 
         if (response.getStatusCode().value() == 200) {
-            tokenStore.setToken(response.getBody().toString());
+            tokenStore.setToken(jsonObject.getJSONObject("data").getString("token"));
             System.out.println("token: " + tokenStore.getToken());
-
-
-        }else {
-            System.out.println("ERROR EN INGRESAR MALA SUERTE PAPITO");
         }
 
+        model.addAttribute("token", tokenStore.getToken());
+        model.addAttribute("user", user);
 
-        // ahora se le manda a la url que necesitamos para demostrar que se usa el token
-
-
-
-
-        return "holamundo";
-    }
-
-    @CrossOrigin(origins = "*")
-    @PostMapping("/testing")
-//    public String testingMethod2() {
-    public String testingMethod(@RequestParam(name = "correo")String email) {
-//        System.out.println("EN LOGIN DE PRUEBA" );
-        System.out.println("EN LOGIN DE PRUEBA " + email);
-        return "holamundo";
-    }
-
-    @GetMapping("/testing")
-    public String testingMethod() {
-        System.out.println("EN LOGIN DE PRUEBA");
-
-        return "holamundo";
+        return "home";
     }
 }
--- src/main/resources/static/js/loginService.js
+++ src/main/resources/static/js/loginService.js
@@ -1,16 +1,3 @@
-
-
-const usuariosRegistrados = [
-    {
-        email: "pe.falfan@duocuc.cl",
-        password: "123456",
-    },
-    {
-        email: "radahn@duocuc.cl",
-        password: "123456",
-    },
-]
-
 const email = {email:""};
 
 const password = {password:""};
@@ -20,51 +7,22 @@ function ingresar() {
     const email = document.getElementById('email').value.toLowerCase();
     const password = document.getElementById('password').value;
 
-
     if (email === "" || password === "") {
         window.alert("Debe completar ambos campos!")
     } else {
 
-        const result = usuariosRegistrados.filter((usuario) => usuario.email === email && usuario.password === password)
+        fetch("/ingresar?email="+email+"&password="+password, {
+            method: 'POST' ,
+            headers: {
+                'content-Type': 'application/json'
+            },
+            body:{}
 
-        console.log("encontrados: " + result.length);
-
-        if (result.length === 1) {
-            window.alert("Bienvenido "  + email + "!")
-
-            const data = JSON.stringify({email:email, password: password});
-
-            console.log("Resp del server: " + data);
-
-
-            fetch("/testing", {
-                method: 'GET' ,
-                headers: {
-                    'content-Type': 'application/json'
-                },
-
-            }, ).then(response => response.text())
-                .then(data => {
-                    console.log("Resp del server: " + data);
-                })
-                .catch(error => console.error('Error: ' + error));
-
-            // $.ajax({
-            //     url: 'http://localhost:8080/testing?correo=pe.falfan@duocuc.cl',
-            //     type: 'POST',
-            //     dataType: 'json',
-            //     success: function (res) {
-            //         console.log(res);
-            //     }
-            // })
-
-            // window.location.replace('http://localhost:8080/testing?correo=pe.falfan@duocuc.cl')
-
-
-
-        } else {
-            window.alert("Comprueba tus credenciales!")
-        }
+        }, ).then(response => response.text())
+            .then(data => {
+                console.log("Resp del server: " + data);
+            })
+            .catch(error => console.error('Error: ' + error));
     }
 
 }
